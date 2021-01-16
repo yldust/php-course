a84af04cdb46d83e3e8f97547d404684e92e28e0
Julia Trifonova <yldust@yandex.ru>
2021-01-16 17:13:58 +0300

    "dz0 is completed"

---
+++ dz0_declaration.php
@@ -0,0 +1,19 @@
+<?php
+//1.Приведите этот код к соответствию стандарту PSR-2
+/*
+const testConstanta = 'asd';
+$user_name = "Igor";
+
+function show_something()
+{
+    return 'SOMETHING';
+}
+
+if (1 == 1) echo 'hi';
+*/
+const TEST_CONSTANTA = 'asd';
+
+function showSomething()
+{
+    return 'SOMETHING';
+}
---
+++ dz0_logic.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'dz0_declaration.php';
+$userName = 'Igor';
+
+if (1 == 1) {
+    echo 'hi';
+}
